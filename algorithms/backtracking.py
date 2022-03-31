55a3a6ff5d744d682bb23b0ffe6b669ac9d9eff7
Garvit <garvit1608@gmail.com>
2022-03-31 10:25:28 +0100

    updates in backtracking algo

--- algorithms/backtracking.py
+++ algorithms/backtracking.py
@@ -1,8 +1,8 @@
 # Function to create N x N board
-def create_board(num_queens):
+def create_board(number_of_queens):
     board = []
-    for i in range(0, num_queens):
-        board.append(['_'] * num_queens)
+    for i in range(0, number_of_queens):
+        board.append(['_'] * number_of_queens)
     return board
 
 # Function to print N x N board
@@ -11,7 +11,7 @@ def print_board(board):
         print(str(row).replace(',', ' ').replace('\'', ''))
     print()
 
-# Function to check if two queens threaten each other or not
+# Function to check if two queens are conflicting each other or not
 def is_safe_move(board, row, col):
 
     # return false if two queens share the same column
@@ -20,36 +20,38 @@ def is_safe_move(board, row, col):
             return False
 
     # return false if two queens share the same primary diagonal
-    (i, j) = (row, col)
-    while i >= 0 and j < len(board):
-        if board[i][j] == 'Q':
+    r = row
+    c = col
+    while r >= 0 and c < len(board):
+        if board[r][c] == 'Q':
             return False
-        i -= 1
-        j += 1
+        r -= 1
+        c += 1
 
     # return false if two queens share the same secondary diagonal
-    (i, j) = (row, col)
-    while i >= 0 and j >= 0:
-        if board[i][j] == 'Q':
+    r = row
+    c = col
+    while r >= 0 and c >= 0:
+        if board[r][c] == 'Q':
             return False
-        i -= 1
-        j -= 1
+        r -= 1
+        c -= 1
 
     return True
 
 def n_queen(board, row):
 
-    # if `N` queens are placed successfully, print the board
+    # if 'N' queens are placed successfully i.e all rows are explored, print the board
     if row == len(board):
         print_board(board)
         return
 
-    # place queen at every column in the current row `row` and recursively call for each valid movement
+    # place queen at every column in the current row 'row' and recursively call for each valid movement
     for col in range(len(board)):
 
         # if no two queens threaten each other
         if is_safe_move(board, row, col):
-            # place queen on the current square
+            # place queen on the current square in board
             board[row][col] = 'Q'
 
             # recursively call for the next row
@@ -61,11 +63,10 @@ def n_queen(board, row):
 
 if __name__ == '__main__':
 
-    # `N × N` chessboard
-    num_queens = int(input('Enter number of queens, N \n'))
+    number_of_queens = int(input('Enter number of queens, N \n'))
 
-    if num_queens <= 3:
+    if number_of_queens <= 3:
         print("Solution does not exist")
     else:
-        board = create_board(num_queens)
+        board = create_board(number_of_queens)
         n_queen(board, 0)
