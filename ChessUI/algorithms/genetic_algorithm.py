46703e6ba204cf740a4d46ac25f117cf579eb370
manupmanoos <37665505+manupmanoos@users.noreply.github.com>
2022-04-09 23:01:02 +0100

    Slightly improved performance

--- ChessUI/algorithms/genetic_algorithm.py
+++ ChessUI/algorithms/genetic_algorithm.py
@@ -64,7 +64,7 @@ def crossover(x, y): #Re-arrange the position in the matrix
 
 def generate_solutionSet(setOfAllSolutions, pairOfNonAttackingQueens,maxPairOfNonAttackingQueens,alter_probability= 0.1): #Populate or generate the list of solutions
     new_SetOfSolutions = []
-    #return_solution =[]
+    return_sol =[]
     probability_vals = []
     for solution in setOfAllSolutions:
         probability_vals.append(pairOfNonAttackingQueens(solution,maxPairOfNonAttackingQueens) / maxPairOfNonAttackingQueens)
@@ -79,10 +79,10 @@ def generate_solutionSet(setOfAllSolutions, pairOfNonAttackingQueens,maxPairOfNo
         
         new_SetOfSolutions.append(child)
         if pairOfNonAttackingQueens(child,maxPairOfNonAttackingQueens) == maxPairOfNonAttackingQueens:
-            #return_solution.append(child)
+            return_sol.append(child)
             break
         i=i+1
-    return new_SetOfSolutions #,return_solution
+    return new_SetOfSolutions, return_sol
 
 def print_arrangement(pos,maxPairOfNonAttackingQueens): #For every position in each fitness (Pair of non-attacking queens), show
     print("Position = {},  Fitness Score = {}"
@@ -111,24 +111,29 @@ def main(num_queen):
         print("Solution does not exist")
     else: 
         setOfAllSolutions = []
-        return_solution =[]
         for i in range(100):
             setOfAllSolutions.append(random_space(num_queen))
 
         maxPairOfNonAttackingQueens = (num_queen*(num_queen-1))/2 
         generation_count =1
+        return_solution =[]
         while [pairOfNonAttackingQueens(pos,maxPairOfNonAttackingQueens) for pos in setOfAllSolutions].count(maxPairOfNonAttackingQueens) <1:
 
-            setOfAllSolutions = generate_solutionSet(setOfAllSolutions, pairOfNonAttackingQueens,maxPairOfNonAttackingQueens)
+            setOfAllSolutions, return_sol = generate_solutionSet(setOfAllSolutions, pairOfNonAttackingQueens,maxPairOfNonAttackingQueens)
             generation_count +=1
 
-        
-        for item in setOfAllSolutions:
-            if pairOfNonAttackingQueens(item,maxPairOfNonAttackingQueens) == maxPairOfNonAttackingQueens:
-                print("")
-                print("Solution found in {}th generation is: ".format(generation_count))
-                return_solution.append(item)
-                print_arrangement(item,maxPairOfNonAttackingQueens)
+        if generation_count == 1:
+            for item in setOfAllSolutions:
+                if pairOfNonAttackingQueens(item,maxPairOfNonAttackingQueens) == maxPairOfNonAttackingQueens:
+                    print("")
+                    print("Solution found in {}th generation is: ".format(generation_count))
+                    return_solution.append(item)
+                    print_arrangement(item,maxPairOfNonAttackingQueens)
+        else:
+            print("")
+            print("Solution found in {}th generation is: ".format(generation_count))
+            return_solution.append(return_sol[0])
+            print_arrangement(return_sol[0],maxPairOfNonAttackingQueens)
         #for pos in setOfAllSolutions:
             #if pairOfNonAttackingQueens(pos,maxPairOfNonAttackingQueens) == maxPairOfNonAttackingQueens:
                 #print("")
