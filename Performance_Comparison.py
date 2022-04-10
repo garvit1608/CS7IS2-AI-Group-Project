e5190d9e548495983e9a21d6b910a28a3704f3db
rohan-girotra <rohan.girotra99@gmail.com>
2022-04-10 21:24:20 +0100

    Final Comparison plot

--- Performance_Comparison.py
+++ Performance_Comparison.py
@@ -2,13 +2,14 @@ from  algorithms import backtracking, hillclimbing, genetic_algorithm,baseline
 import numpy as np
 from matplotlib import pyplot as plt
 
+queen_range=11
 N_queen=[]
 Hill_time=[]
 Genetic_time=[]
 backtracking_time=[]
 baseline_time=[]
 
-for i in range(4,6):
+for i in range(4,queen_range):
     N_queen=np.append(N_queen,i)
     Hill_time= np.append(Hill_time,hillclimbing.main(i))
     Genetic_time= np.append(Genetic_time,genetic_algorithm.main(i))
@@ -16,12 +17,14 @@ for i in range(4,6):
     baseline_time = np.append(baseline_time,baseline.main(i))
 
 
+
+plt.xticks(np.arange(min(N_queen), max(N_queen)+1, 1))
+plt.plot(N_queen,baseline_time,label="Baseline")
 plt.plot(N_queen,Hill_time,label="Hill Climbing")
+plt.plot(N_queen,backtracking_time,label="Backtracking")
 plt.plot(N_queen,Genetic_time,label="Genetic algorithm")
-plt.plot(N_queen,backtracking_time,label="Back tracking")
-plt.plot(N_queen,baseline_time,label="Baseline algorithm")
 plt.xlabel('Number of Queens')
-plt.ylabel('Time taken in milliseconds')
-plt.legend(loc='upper right')
+plt.ylabel('Time in milliseconds')
+plt.legend(loc='upper left')
 plt.title('Algorithm performance')
 plt.show()
